2c9f8829b702d35594bd9e730400800b3b3bc4f4
Ricardo <you@example.com>
2024-04-06 20:08:07 +0800

    updates

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import OverlayManager, {addOverlay, delOverlay, clearOverlays, delChildOverlays} from "./OverlayManager.jsx"
+
+export {addOverlay, delOverlay, clearOverlays, delChildOverlays}
+
+export default OverlayManager
