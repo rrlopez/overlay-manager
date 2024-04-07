417ce81d87138bb9382bf9089007ea0f657089bc
Ricardo <you@example.com>
2024-04-07 19:17:55 +0800

    added examples

---
+++ examples/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (<div className="w-screen h-screen flex items-center justify-center">
+    <h1 className="btn">
+      Click Me
+    </h1>
+  </div>)
+}
+
+export default App
