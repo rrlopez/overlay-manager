c7d299ea87090cbb94c55ddba65a51dd01e76496
Ricardo <you@example.com>
2024-04-14 10:40:17 +0800

    setup storybook

---
+++ examples/tailwind.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
